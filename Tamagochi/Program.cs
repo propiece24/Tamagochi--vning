81ae7da68c9c3d43a5ebfdf59f5328049b5b4ac0
Andreas <propiece02@gmail.com>
2020-10-06 10:18:20 +0200

    commit

---
+++ Tamagochi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tamagochi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
